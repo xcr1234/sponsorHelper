90abecc2ef3d42b6bdae321f04783f584c307440
xcr_1 <530551426@qq.com>
2026-02-05 00:25:33 +0800

    add test（提取字幕，后面优化成从字幕获取）

--- test.py
+++ test.py
@@ -15,5 +15,6 @@ async def main():
     pprint.pp(res)
 
 if __name__ == '__main__':
+    # 参考资料：https://www.msn.cn/zh-cn/news/other/%E8%A7%81%E4%B8%8D%E6%83%AFb%E7%AB%99up%E4%B8%BB%E7%9A%84%E5%B8%A6%E8%B4%A7%E5%B9%BF%E5%91%8A-%E6%95%99%E4%BD%A0%E5%88%A9%E7%94%A8ai%E5%BF%AB%E9%80%9F%E8%B7%B3%E8%BF%87/ar-AA1VCxW4?ocid=msedgdhp&pc=U531&cvid=6983691241084fce9ed37425b24a7dcf&ei=6
     import asyncio
     asyncio.run(main())
