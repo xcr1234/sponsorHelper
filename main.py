7937975100b2d8c8478f7130b10ec7ed96bf3b37
xcr_1 <530551426@qq.com>
2026-02-05 23:40:10 +0800

    优化

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import asyncio
+import sys
 
 from loguru import logger
 from src.credential import init as init_credential
 from src.db import init as init_db
 from src.main import run_per_loop
 
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    backtrace=False,
+    diagnose=True,
+)
 
 async def main():
     init_db()
